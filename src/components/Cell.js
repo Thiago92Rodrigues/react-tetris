a911d90b20068287e6626e2c073a70493725b9b3
Thiago92Rodrigues <thiago.clrodrigues@gmail.com>
2020-02-22 15:20:29 -0300

    fix typo and imports

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -7,4 +7,5 @@ const Cell = ({ type }) => (
   <StyledCell type={type} color={TETROMINOS[type].color} />
 );
 
+// React.memo makes sure we only re-render the changed cells
 export default React.memo(Cell);
--- src/components/styles/StyledTetris.js
+++ src/components/styles/StyledTetris.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const StyledTetrisWrapper = styled.div`
   width: 100vw;
   height: 100vh;
-  background: url('/public/images/background.png') #000;
+  background: url('/images/background.png') #000;
   background-size: cover;
   overflow: hidden;
 `;
