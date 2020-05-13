11a8f781fa051cf8e7cecec1a63c3b51d26f9663
Thiago92Rodrigues <thiago.clrodrigues@gmail.com>
2020-05-13 19:20:41 -0300

    in case of undefined tetrominos type set the zero type

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -3,9 +3,12 @@ import React from 'react';
 import { StyledCell } from './styles/StyledCell';
 import { TETROMINOS } from '../tetrominos';
 
-const Cell = ({ type }) => (
-  <StyledCell type={type} color={TETROMINOS[type].color} />
-);
+const Cell = ({ type }) => {
+  if (TETROMINOS[type] === undefined) {
+    type = 0;
+  }
+  return <StyledCell type={type} color={TETROMINOS[type].color} />;
+};
 
 // React.memo makes sure we only re-render the changed cells
 export default React.memo(Cell);
